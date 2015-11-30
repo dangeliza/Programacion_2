da3b4010db4468e37953c183a5268a162b166f5e
dangeliza <jruizutn@gmail.com>
2015-11-29 22:31:35 -0600

    Update corridaErr.java

--- src/corridaErr.java
+++ src/corridaErr.java
@@ -13,29 +13,21 @@ public class corridaErr {
         int val1=0, val2=0, resu = 0;
         int vec[] = new int[4];
         try{
-            val1 = Integer.parseInt(JOptionPane.
-                    showInputDialog("Digite un número"));
+            val1 = Integer.parseInt(JOptionPane.showInputDialog("Digite un número"));
             
-            val2 = Integer.parseInt(JOptionPane.
-                    showInputDialog("Digite otro número"));
+            val2 = Integer.parseInt(JOptionPane.showInputDialog("Digite otro número"));
             
             resu = val1 / val2;
             vec[4] = resu;
             
         }catch(NumberFormatException ex){
-            JOptionPane.showMessageDialog(null,
-                    "Error: "+ex.getMessage());
+            JOptionPane.showMessageDialog(null, "Error: "+ex.getMessage());
         }catch(ArithmeticException ex){
-            JOptionPane.showMessageDialog(null,
-                    "Error: "+ex.getMessage());
+            JOptionPane.showMessageDialog(null, "Error: "+ex.getMessage());
         }catch(ArrayIndexOutOfBoundsException ex){
-            JOptionPane.showMessageDialog(null,
-                    "Error: Accediendo al vector en la posición "+
-                            ex.getMessage());
+            JOptionPane.showMessageDialog(null, "Error: Accediendo al vector en la posición " + ex.getMessage());
         }finally{
-            JOptionPane.showMessageDialog(null,
-                    "El resultado es: " + resu);
-        }        
-        
+            JOptionPane.showMessageDialog(null, "El resultado es: " + resu);
+        } 
     }//fin del metodo principal
 }//fin de la clase de prueba de errores.
